4ae1f1b31721c16cd09bb2f72327c757c85684fb
Undiscovered-Data <chaserbrummel@gmail.com>
2021-04-09 00:13:51 -0500

    Update check.py

--- covid_poly_a/one_command/check.py
+++ covid_poly_a/one_command/check.py
@@ -4,7 +4,7 @@ the_file = input("Enter the file name,\nfor example Covid_55.fasta   ")
 the_count = 0
 comp = open(the_file, 'r')
 for line in comp:
-    if '>' in line:
+    if line.startswith('>'):
         the_count += 1
         if the_count % 10000 == 0:
             print("{} viruses so far...".format(the_count))
